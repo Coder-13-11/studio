b86c7a9e24ec38c70fc56a30138e33735a8b85aa
Srinivasam Yeruva <ysr.aws@gmail.com>
2025-10-25 19:01:42 +0000

    the signup page redirects me back to the main page. It seems there is an

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,6 +2,7 @@
 import { ReactNode, useEffect } from "react";
 import { useUser } from "@/firebase";
 import { redirect } from "next/navigation";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function AuthLayout({ children }: { children: ReactNode}) {
     const { user, isUserLoading } = useUser();
@@ -15,7 +16,10 @@ export default function AuthLayout({ children }: { children: ReactNode}) {
     if(isUserLoading || user){
         return (
              <div className="flex h-screen w-full items-center justify-center">
-                <p>Loading...</p>
+                <div className="flex flex-col items-center gap-4">
+                  <Skeleton className="h-12 w-12 rounded-full" />
+                  <Skeleton className="h-4 w-48" />
+                </div>
             </div>
         )
     }
